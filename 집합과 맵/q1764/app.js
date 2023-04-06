188739464b31cb4a62f90da9f07363f2cafbfbc9
gunhee94 <erjsgml94@naver.com>
2023-04-06 15:34:56 +0900

    집합과 맵

--- 집합과 맵/q1764/app.js
+++ 집합과 맵/q1764/app.js
@@ -7,3 +7,20 @@ let input = fs
     .trim()
     .split("\n")
     .map((e) => e);
+
+let [listen, see] = input[0].split(" ").map((e) => Number(e));
+
+let set = new Set();
+for (let i = 1; i <= listen; i++) {
+    set.add(input[i]);
+}
+
+let arr = [];
+for (let i = listen + 1; i <= listen + see; i++) {
+    if (set.has(input[i])) {
+        arr.push(input[i]);
+    }
+}
+arr.sort();
+console.log(arr.length);
+arr.length !== 0 && console.log(arr.join("\n"));
